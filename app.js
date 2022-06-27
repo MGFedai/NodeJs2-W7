abf68d8523d129124142162537184a9673cd9200
MGF <m.g.fedai@gmail.com>
2022-06-27 11:43:31 +0300

    CleanBlogW2 first Commit

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const ejs =require('ejs');
+const path = require("path");
 
 const app = express();
-app.get('/',(req,res)=>{
+//TEMPLATE ENGİNE
+app.set("view engine", "ejs");
 
-    const blog = {
-        id:1,
-        name :'Blog Title',
-        description:'Blog Description'
-    }
-    res.send(blog)
-})
+// MIDDLEWARES
+app.use(express.static("public"));
+// ROUTES
+app.get("/", (req, res) => {
+  // dosyayo içeri çektik
+  // express static middleware kullandık
+  //res.sendFile(path.resolve(__dirname, "tmp/index.html"));
+  res.render('index')
+});
+app.get("/about", (req, res) => {
+  res.render('about')
+});
+app.get("/post", (req, res) => {
+  res.render('post')
+});
+app.get("/contact", (req, res) => {
+  res.render('contact')
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda başlatıldı`);
