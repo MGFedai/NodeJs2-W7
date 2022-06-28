c06c22837478a9027d4e9940ba7f04249c09a13c
MGF <m.g.fedai@gmail.com>
2022-06-28 15:56:29 +0300

    ödev10 a göre düzenlenmiştir.

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const ejs =require('ejs');
 const path = require("path");
-
+const post = require('./models/newPost');
 const app = express();
+
+// connect  DB
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 //TEMPLATE ENGİNE
 app.set("view engine", "ejs");
 
 // MIDDLEWARES
-app.use(express.static("public"));
+app.use(express.static('public')); // Static dosyaları koyacağımız klasörü seçtik
+app.use(express.urlencoded({ extended: true })); // Body parser okuyoruz
+app.use(express.json()); // Body parser dönüştürüyoruz
 // ROUTES
-app.get("/", (req, res) => {
+app.get('/posts/:id',async (req, res) => {   
+  const addPost = await post.findById(req.params.id)
+    res.render('post',{
+      addPost
+    });
+});
+
+
+
+
+app.get("/", async (req, res) => {
+  const addPost = await post.find({})
   // dosyayo içeri çektik
   // express static middleware kullandık
   //res.sendFile(path.resolve(__dirname, "tmp/index.html"));
-  res.render('index')
+  res.render('index',{
+    addPost
+  })
+ 
 });
+
+
 app.get("/about", (req, res) => {
   res.render('about')
 });
@@ -24,6 +47,21 @@ app.get("/post", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render('contact')
 });
+app.get("/addNewPost", (req, res) => {
+  res.render('addNewPost')
+});
+app.post('/photos', async (req, res) => { // async - await yapısı kullanacğız.
+  await post.create(req.body)// body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+  res.redirect('/')
+});
+
+app.get('/', async (req, res) => {
+  const addPost = await posts.find({})
+  res.render('index', {
+    addPost
+  })});
+
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda başlatıldı`);
